8c2ae9f374608e5557ba5abce16ef9674ddf48c8
Hamdan Salih <hamdansalihpc@gmail.com>
2023-02-07 21:44:24 +0530

    adding items to cart from shop page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from 'react-router-dom';
 import { Route } from 'react-router-dom';
+import { useState, useEffect } from 'react'; 
 import './App.css';
 import Cart from './Components/Cart';
 import Contact from './Components/Contact';
@@ -10,6 +11,20 @@ import Nav from './Components/Nav';
 import Shop from './Components/Shop';
 
 function App() {
+  const [CartItems, setCartItems] = useState('');
+  
+  useEffect(() => {
+    console.log(CartItems);
+  }, [CartItems]);
+
+  const getItems = (newItem) =>{
+    if(CartItems === ''){
+      setCartItems([newItem])
+    }else{
+      setCartItems([...CartItems, newItem])
+    }
+  }
+  
   return (
     <div className='app'>
       <BrowserRouter>
@@ -18,7 +33,7 @@ function App() {
           <Route path='/' exact element={<Home />} />
           <Route path='/cart' exact element={<Cart />} />
           <Route path='/contact' exact element={<Contact />} />
-          <Route path='/shop' exact element={<Shop />} />
+          <Route path='/shop' exact element={<Shop passData={getItems} />} />
           <Route path='/shop/:id' exact element={<Item />} />
 
         </Routes>
--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -5,15 +5,15 @@ import axios from 'axios'
 import "./Shop.css"
 import { Link } from 'react-router-dom'
 
-function Shop() {
+function Shop(props) {
 
   const [Books, setBooks] = useState([]);
+  const [Item, setItem] = useState('');
 
   useEffect(() => {
   
     axios.get(`https://www.googleapis.com/books/v1/volumes?q=subject:fiction&maxResults=20&key=AIzaSyDsK20p3fvvNkpgE4xwfasAYMdcvlVRKTI`    )
       .then(res => {
-        console.log(res.data.items);
         setBooks(res.data.items)
       })
       .catch(err => {
@@ -21,14 +21,23 @@ function Shop() {
       });
   }, []);
 
-  const handleAddCart = (e)=>{
-    e.preventDefault()
-    console.log("hii clicked");
+  useEffect(() => {
+    if(Item === ''){
+      console.log('no items')
+    }else{
+      props.passData(Item)
+    }
+  }, [Item]);
+
+
+  const handleAddCart = (item)=>{
+    console.log(item);
+    setItem(item)
   }
 
   return (
     <div className='shop-list'>
-       {Books.map((book)=>{
+       {Books.map((book , index)=>{
           const bookInfo = book.volumeInfo;
           let bookRating ="";
           if(bookInfo.averageRating){
@@ -39,14 +48,14 @@ function Shop() {
           const bookPrice = (bookRating*2.5)  
           return (
             <Link to={`/shop/${book.id}`}>
-              <div className='book-card' >
+              <div className='book-card' key={index}>
                 <img src={bookInfo.imageLinks.thumbnail} alt="" />
                 <h3>{bookInfo.title}</h3>
                 <div>
                   <p>Rating :{bookRating}</p>
                   <p>${bookPrice}</p>
                 </div>
-                <button onClick={handleAddCart}>Add To Cart</button>
+                <button onClick={(e)=>{e.preventDefault();handleAddCart(book)}}>Add To Cart</button>
               </div>
             </Link>
           )
