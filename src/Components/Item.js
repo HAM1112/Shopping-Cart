f2aede06ee1df349f115b2f2adea7333a4797b4b
Hamdan Salih <hamdansalihpc@gmail.com>
2023-02-08 23:01:46 +0530

    Item details displayed and creted Add Cart button witei

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -5,41 +5,54 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import "./items.css"
 
-function Item({match}) {
+function Item(props) {
   const {id} = useParams();
   const [Book, setBook] = useState('');
+  const [Item, setItem] = useState('');
+  
+
 
   useEffect(() => {
     axios.get(`https://www.googleapis.com/books/v1/volumes/${id}?key=AIzaSyDsK20p3fvvNkpgE4xwfasAYMdcvlVRKTI`    )
       .then(res => {
-        console.log(res.data);
-
+        // console.log(res.data);
         setBook(res.data)
       })
       .catch(err => {
         console.error(err);
       });
-      console.log(id);
   },[]);
 
+  useEffect(() => {
+    if(Item === ''){
+      console.log('no items')
+    }else{
+      props.passData(Item)
+    }
+  }, [Item]);
 
+  const handleAddCart = (item)=>{
+    console.log(item);
+    setItem(item)
+  }
 
   return (
-    <div >
-      
+
+
+    <div >  
         {Book? <div className='book-display'>
           <div className='book-image'>
-            <img src={Book.volumeInfo.imageLinks.thumbnail} alt="" srcset="" />
+            <img src={Book.volumeInfo.imageLinks.thumbnail} alt='image'  />
           </div>
           <div className='book-details'>
             <h1>{Book.volumeInfo.title}</h1>
-            <p className='authors-details'>{(Book.volumeInfo.authors).map((author)=>{return <div>{author}</div> })}</p>
+            <p className='authors-details'>Author : {(Book.volumeInfo.authors).map((author)=>{return <p>{author}</p> })}</p>
             <p>Pages : {Book.volumeInfo.pageCount}</p>
-            <p>Date ofpublication : {Book.volumeInfo.publishedDate}</p>
+            <p>Date of publication : {Book.volumeInfo.publishedDate}</p>
             <p>Published by : {Book.volumeInfo.publisher}</p>
-            <p>Rating : {Book.volumeInfo.averageRating}</p>
-           <span>price</span>
-          
+            {Book.volumeInfo.averageRating ? <p>Rating : {Book.volumeInfo.averageRating}</p> : <p>Rating : 3.8</p> }
+            {Book.volumeInfo.averageRating ? <p>Price : ${(Book.volumeInfo.averageRating)*2.5}</p> : <p>Price : {(3.8)*2.5}</p> }
+            <button onClick={(e)=>{e.preventDefault();handleAddCart({title: Book.volumeInfo.title , imageLink: Book.volumeInfo.imageLinks.thumbnail, id:Book.id})}}>Add to Cart</button>
           </div>
         </div> : <div className='loading'>Loading.....</div> }
       
